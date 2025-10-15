d420990ddd549c72399e193ba13283e1d2599138
shivailan <shiva77127@gmail.com>
2025-10-15 11:17:13 +0200

    Ajout du composant InputLogger avec gestion des événements React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import InputLogger from "./components/InputLogger";
+
+function App() {
+  return (
+    <div>
+      <h1>ReactBook - Partie 3</h1>
+      <InputLogger />
+    </div>
+  );
+}
+
+export default App;
