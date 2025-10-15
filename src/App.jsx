189a0ca97b9eed7eb55c7d804a8ac936e52032dc
shivailan <shiva77127@gmail.com>
2025-10-15 11:24:12 +0200

    Ajout du LoginForm et finalisation InputLogger pour la Partie 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import InputLogger from "./components/InputLogger";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <div>
       <h1>ReactBook - Partie 3</h1>
       <InputLogger />
+      <LoginForm />
     </div>
   );
 }
